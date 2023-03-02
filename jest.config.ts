ff875d3975421c27534a687403d0fe7921656b1a
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-02 22:49:44 +0300

    test(jest): - add script test; - change jest.config.ts; - chenge .eslintrc.json
    meta: issue-22

--- jest.config.ts
+++ jest.config.ts
@@ -17,18 +17,20 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '**/src/**/*.tsx',
+    '**/src/**/*.ts',
+    '!**/src/**/*.stories.tsx',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index.ts', 'src/types'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
